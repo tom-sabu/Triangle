a51c4b5729846626ff9bf916b77ac0115ed3693e
Tom <tomsabu@tutamail.com>
2026-03-19 16:35:51 +0530

    created shader modules and shader stage!

--- src/VulkanPipeLine.cpp
+++ src/VulkanPipeLine.cpp
@@ -19,7 +19,6 @@ static std::vector<char> readFile(const std::string &filename) {
 
   file.seekg(0);
   file.read(buffer.data(), fileSize);
-
   file.close();
 
   return buffer;
@@ -28,4 +27,35 @@ static std::vector<char> readFile(const std::string &filename) {
 void VulkanPipeLine::createGraphicsPipeline() {
   auto vertShaderCode = readFile("Shaders/vert.spv");
   auto fragShaderCode = readFile("Shaders/frag.spv");
+
+  VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+  VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+  VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+  vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+  vertShaderStageInfo.module = vertShaderModule;
+  vertShaderStageInfo.pName = "main";
+
+  VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+  fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+  fragShaderStageInfo.module = fragShaderModule;
+  fragShaderStageInfo.pName = "main";
+
+  VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+}
+
+VkShaderModule VulkanPipeLine::createShaderModule(const std::vector<char>& code) {
+  VkShaderModuleCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+  createInfo.codeSize = code.size();
+  createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+  VkShaderModule shaderModule;
+  if (vkCreateShaderModule(device.getDevice(), &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create shader module!");
+  }
+
+  return shaderModule;
 }
